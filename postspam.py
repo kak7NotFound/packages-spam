7dd2e68af3b736f88c40ffd97682bb8916a5d93f
kak7 <kaka0monstr@gmail.com>
2021-07-27 23:44:44 +0300

    1

--- postspam.py
+++ postspam.py
@@ -11,6 +11,30 @@ url = 'https://discod.gift/login/dologin'
 useragents = open("useragents.txt", "r", encoding='UTF-8').read()
 
 
+def create_get_request():
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Host': 'discod.gift',
+        'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+        'sec-ch-ua-mobile': '?0',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'}
+
+    data = requests.get('https://discod.gift/steam', headers=headers)
+    data = data.headers.__dict__.get('_store')
+
+    # Работа программиста и шамана имеет много общего — оба бормочут непонятные слова, совершают непонятные действия и не могут объяснить, как оно работает.
+    return data.get('set-cookie')[1].split(';')[0].split('=')[1]
+
+
 def generate_headers(username, password):
     headers = {'Host': 'discod.gift',
                'User-Agent': f'{get_random_useragent() + str(random.randrange(0, 10, 1))}',
@@ -23,8 +47,8 @@ def generate_headers(username, password):
                'Origin': 'https://discod.gift',
                'DNT': '1',
                'Connection': 'keep-alive',
-               'Referer': 'https://discod.gift/3WUpPRLxtCuFWdbd4a5a8d2Jt4T5rbnAr51FGVdWpnItFkAoklXmROwQr',
-               'Cookie': 'lumen_session=gBoj5ncX2RKnbMkPXU3SRCojHhHqOWlU67EhaiaO; _TDG=919ebdb693dd4e55a2c4e1cb7d87fefe; timezoneOffset=10800,0',
+               'Referer': 'https://discod.gift/ioaetdgnmiae65tgniu6niea6gniae6gneia6tngaikjunhbiakrha6yu',
+               'Cookie': f'lumen_session={create_get_request()}; _TDG=SashaWas0nTh3H1ghwa9And5uckedDrying; timezoneOffset=10800,0',
                'Sec-Fetch-Dest': 'empty',
                'Sec-Fetch-Mode': 'cors',
                'Sec-Fetch-Site': 'same-origin',
@@ -45,6 +69,7 @@ def get_random_userdata():
 
 
 def create_post():
+
     u = get_random_userdata()["username"]
     p = get_random_userdata()["password"]
     r = requests.post(url, data=f'username={u}&password={p}', headers=generate_headers(u, p))
@@ -54,4 +79,4 @@ def create_post():
 # r = requests.post(url, data=f'username={username}&password={password}', headers=headers)
 while True:
     print(create_post())
-    time.sleep(3)
+    time.sleep(2)
